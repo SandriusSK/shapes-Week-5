1b4009ccee0d1524aeb12a3c9bd33a3e5a39e16e
g00402163 <g00402163@atu.ie>
2022-10-24 09:35:32 +0100

    added file called rectangle

---
+++ src/main/java/ie/atu/week5/rectangle.java
@@ -0,0 +1,25 @@
+package ie.atu.week5;
+
+import java.util.Scanner;
+
+public class rectangle {
+    private double lenght;
+    private double width;
+
+    public double getLenght() {
+        return lenght;
+    }
+
+    public void setLenght(double lenght) {
+        this.lenght = lenght;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
+
